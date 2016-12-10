25fcc7fb190b99d67e2f0e127d8b1f9d668e1f5f
Simon Hamermesh <simon@hamermesh>
2016-12-10 02:16:47 -0500

    Initial Commit

---
+++ js/recipes.js
@@ -0,0 +1,52 @@
+var recipes = [
+{
+name: "cookie",
+main: "chocolate",
+time: 25	
+},
+{
+name: "cookie",
+main: "sugar",
+time: 22	
+},
+{
+name: "cookie",
+main: "dark chocolate",
+time: 19	
+},
+{
+name: "cookie",
+main: "peanut butter",
+time: 23	
+},
+{
+name: "cake",
+main: "chocolate",
+time: 45	
+},
+{
+name: "cake",
+main: "vanilla",
+time: 37	
+},
+{
+name: "cake",
+main: "red",
+time: 52	
+},
+{
+name: "pasta",
+main: "ravioli",
+time: 5	
+},
+{
+name: "pasta",
+main: "macaroni",
+time: 12	
+},
+{
+name: "pasta",
+main: "fusilli",
+time: 12	
+}
+]
