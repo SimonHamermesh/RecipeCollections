59d029c5a90bc77e34725e898ec13e3cf7e14a98
Simon Hamermesh <simon@hamermesh>
2016-12-10 02:33:40 -0500

    omg am i a lucky sob

---
+++ recipes.js
@@ -0,0 +1,98 @@
+var recipes = [
+{
+	"type": "cookie",
+	"ingredients": {
+		"main": "chocolate",
+		"second": "butter"
+	},
+	"time": 25
+},
+{
+	"type": "cookie",
+	"ingredients": {
+		"main": "sugar",
+		"second": "butter"
+	},
+	"time": 25
+},
+{
+	"type": "cookie",
+	"ingredients": {
+		"main": "oatmeal",
+		"second": "raisin"
+	},
+	"time": 25
+},
+{
+	"type": "cookie",
+	"ingredients": {
+		"main": "chocolate",
+		"second": "peanut butter"
+	},
+	"time": 20
+},
+{
+	"type": "cookie",
+	"ingredients": {
+		"main": "peanut butter",
+		"second": "chocolate"
+	},
+	"time": 25
+},
+{
+	"type": "cookie",
+	"ingredients": {
+		"main": "chocolate",
+		"second": "butter"
+	},
+	"time": 25
+},
+{
+	"type": "pizza",
+	"ingredients": {
+		"main": "cheese",
+		"second": "cheese"
+	},
+	"time": 25
+},
+{
+	"type": "pizza",
+	"ingredients": {
+		"main": "cheese",
+		"second": "pepperoni"
+	},
+	"time": 30
+},
+{
+	"type": "pizza",
+	"ingredients": {
+		"main": "cheese",
+		"second": "anchovies"
+	},
+	"time": 27
+},
+{
+	"type": "pizza",
+	"ingredients": {
+		"main": "cheese",
+		"second": "cheese"
+	},
+	"time": 25
+},
+{
+	"type": "pizza",
+	"ingredients": {
+		"main": "sauce",
+		"second": ""
+	},
+	"time": 18
+},
+{
+	"type": "pizza",
+	"ingredients": {
+		"main": "cheese",
+		"second": ""
+	},
+	"time": 21
+}
+]
